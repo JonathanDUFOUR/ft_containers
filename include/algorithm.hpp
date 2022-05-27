23424f5874c6795b7febf6eba4747a888cc9dc6b
jodufour <jodufour@student.42.fr>
2022-05-27 13:30:59 +0000

    Implement equal                   [OK] Implement lexicographical_compare [TODO]

---
+++ include/algorithm.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   algorithm.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/27 13:09:13 by jodufour          #+#    #+#             */
+/*   Updated: 2022/05/27 13:29:25 by jodufour         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ALGORITHM_HPP
+# define ALGORITHM_HPP
+
+template <typename InputIterator0, typename InputIterator1>
+bool	equal(
+	InputIterator0 first0,
+	InputIterator0 last0,
+	InputIterator1 first1)
+{
+	while (first0 != last0 && *first0 == *first1)
+	{
+		++first0;
+		++first1;
+	}
+	return first0 == last0;
+}
+
+template <typename InputIterator0, typename InputIterator1, typename BinaryPredicate>
+bool	equal(
+	InputIterator0 first0,
+	InputIterator0 last0,
+	InputIterator1 first1,
+	BinaryPredicate pred)
+{
+	while (first0 != last0 && pred(*first0, *first1))
+	{
+		++first0;
+		++first1;
+	}
+	return first0 == last0;
+}
+
+#endif
